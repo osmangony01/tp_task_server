8578c32dfae5ce43bfc80d1216a529d898e4dd6e
osmangony01 <mdosmangoni376@gmail.com>
2023-07-06 01:02:27 +0600

    create a NodeJS server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+
+app.get("/", (req, res) => {
+    res.send("Server is running . . .");
+})
+app.listen(port, () => {
+    console.log(`TP Task Server is running on port: ${port}`);
+})
