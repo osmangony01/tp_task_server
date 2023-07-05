696bd0c03d959a9bb2ee21efbcc0490ad84e1a9d
osmangony01 <mdosmangoni376@gmail.com>
2023-07-06 02:04:08 +0600

    add get route to show all services

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-
+const services = require("./data/Services.json");
 
 app.get("/", (req, res) => {
     res.send("Server is running . . .");
 })
+
+app.get("/services", (req, res) =>{
+    res.send(services);
+})
+
+
 app.listen(port, () => {
     console.log(`TP Task Server is running on port: ${port}`);
 })
